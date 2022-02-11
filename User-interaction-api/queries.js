38819150de2fd53ee4ba2f84a239882bd9180b77
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-02-11 11:31:35 +0530

    interaction finalized

--- User-interaction-api/queries.js
+++ User-interaction-api/queries.js
@@ -3,70 +3,101 @@ const db = require("diskdb");
 db.connect("db", ["interaction"]);
 const axios = require("axios").default;
 
-const addInteractionLike = async (title, story_id,user_id,like) => {
+const addInteractionLike = async ( story_id,user_id,like) => {
     const userFlag = await getUser(user_id);
     if (!userFlag) {
-        return;
+        return 401;
     }
     const Interaction = {
-      title: title,
       story_id: story_id,
       like: like || 0,
       read: 0,
     };
-    db.interaction.save([Interaction]);
-    console.log("data inserted into interactiondb");
+  db.interaction.save([Interaction]);
+  const res = await db.interaction.findOne({
+    story_id: story_id,
+    user_id: user_id,
+    like: like,
+  });
+  console.log("data inserted into interactiondb");
+  return res._id;
 };
-const addInteractionRead = async (title, story_id, user_id, read) => {
+const addInteractionRead = async ( story_id, user_id, read) => {
   const userFlag = await getUser(user_id);
   if (!userFlag) {
-    return;
+    return 401;
   }
   const Interaction = {
-    title: title,
     story_id: story_id,
     like: 0,
     read: read,
   };
   db.interaction.save([Interaction]);
+  const res = await db.interaction.findOne({
+    story_id: story_id,
+    user_id: user_id,
+    read: read,
+  });
   console.log("data inserted into interactiondb");
+  return res._id;
 };
 
 // addInteraction("abc", "xyz", "e3c057fee2824329ae75f88cb75e019b","like");
 
-const updateInteraction = async (story_id, event, flag, user_id) => {
+const updateInteraction = async (_id, event, flag, user_id) => {
   const userFlag = await getUser(user_id);
   if (!userFlag) {
-    return;
+    return 401;
+  }
+  const checkExistence = await db.interaction.findOne({ _id: _id });
+  console.log(checkExistence);
+  if (checkExistence == undefined) {
+    return 404;
   }
   options = {
     multi: false, // update multiple - default false
     upsert: false, // if object is not found, add it (update-insert) - default false
   };
   const query = {
-    story_id: story_id,
+    _id: _id,
   };
   const change = !flag ? -1 : 1;
   const dataToBeUpdate = {
-    [event]: +db.interaction.findOne({ story_id: story_id })[event] + change,
+    [event]: +db.interaction.findOne({ _id: _id })[event] + change,
   };
      const updated = db.interaction.update(query, dataToBeUpdate, options);
- 
-  a = db.interaction.findOne({ story_id: story_id })[event];
-  console.log(`${event}  --->  ${a}`);
-  return 0;
+  const status = updated.updated;
+  if (status) {
+    return {
+      _id: _id,
+      updated: true,
+    };
+  } else {
+    return 500;
+  }
+  
 
 };
-const getInteractions =  () => {
+const getInteractions = () => {
+  // let a = db.interaction.find();
+  // console.log(a);
   return db.interaction.find();
 };
-
+const removeInteractionbyId = async (id) => {
+  const result = await db.interaction.findOne({ _id: id });
+  if (result === undefined) {
+    return 404;
+  } else {
+    const del = await db.interaction.remove({ _id: id });
+    return 200;
+  }
+};
 async function getUser(id) {
+
   try {
-      const response = await axios.get(`http://user:4000/user/${id}`);
+      const response = await axios.get(`http://user:4000/${id}`);
       const { fname, lname, email, phno, _id } = response.data;
       if ((fname && lname && email && phno && _id)) { 
-          console.log(response.data);
           return true;
       }
       else {
@@ -87,10 +118,12 @@ async function getUser(id) {
 //   "e3c057fee2824329ae75f88cb75e019b"
 // );
 
-// 
+//
+// getInteractions(); 
 module.exports = {
   addInteractionRead,
   addInteractionLike,
   updateInteraction,
   getInteractions,
+  removeInteractionbyId,
 };
--- User-interaction-api/routes/manage.js
+++ User-interaction-api/routes/manage.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const {
   addInteractionLike,
-    updateInteraction,
+  updateInteraction,
   addInteractionRead,
   getInteractions,
+  removeInteractionbyId,
 } = require("../queries");
 const routes = express.Router();
 
@@ -30,7 +31,7 @@ const routes = express.Router();
  *           description: The number of like events
  *       example:
  *         story_id: a335c79a626b42fc8fad35b97d11ec71
- *         user_id: e3c057fee2824329ae75f88cb75e019bs
+ *         user_id: 8178eea98d9543a5ae115c94adbc0c4a
  *         like: 1
  */
 
@@ -56,7 +57,7 @@ const routes = express.Router();
  *           description: The number of read events
  *       example:
  *         story_id: a335c79a626b42fc8fad35b97d11ec71
- *         user_id: e3c057fee2824329ae75f88cb75e019bs
+ *         user_id: 8178eea98d9543a5ae115c94adbc0c4a
  *         read: 1
  */
 
@@ -69,7 +70,43 @@ const routes = express.Router();
   *   description: The UserInteraction service API
   */
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     PostRes:
+ *       type: object
+ *       required:
+ *         - id
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: the id of inserted user
+ *       example:
+ *         _id: "5bb74e3eea474bf3ab09ac427bf2cb05"
+ */
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     PutUser:
+ *       type: object
+ *       required:
+ *         - _id
+ *         - updated
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated id of the content
+ *         updated:
+ *           type: boolean
+ *           description: status of update operation
+ *       example:
+ *         _id: 94c7f94a8bae46f290ad5312ad2d07f5
+ *         updated: true
+ * 
+ */
 
 
  /**
@@ -85,17 +122,27 @@ const routes = express.Router();
  *           schema:
  *             $ref: '#/components/schemas/UserLike'
  *     responses:
- *       200:
- *         description: The interaction was successfully added
+ *       201:
+ *         description: The User was successfully added
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/PostRes'
  *       500:
  *         description: Some server error
  */
 
 routes.post('/like', async (req, res) => {
     console.log(req.body);
-    const { title, story_id, user_id, like } = req.body;
-    await addInteractionLike(title, story_id, user_id, like);
-    res.send("Data inserted into Interaction db");
+    const {story_id, user_id, like } = req.body;
+    const result =await addInteractionLike(story_id, user_id, like);
+  if (result===401) {
+    res.status(401).send("User not found Error!!!");
+  }
+  else {
+    const sender = { _id: result };
+    res.status(201).send(sender);
+  }
 });
 
 
@@ -104,7 +151,7 @@ routes.post('/like', async (req, res) => {
  * @swagger
  * /read:
  *   post:
- *     summary: Acknowledges read to particular content and inserts entries into db
+ *     summary: Acknowledges read to particular content and inserts entry into db
  *     tags: [UserInteraction]
  *     requestBody:
  *       required: true
@@ -113,16 +160,29 @@ routes.post('/like', async (req, res) => {
  *           schema:
  *             $ref: '#/components/schemas/UserRead'
  *     responses:
- *       200:
- *         description: The interaction was successfully added
+ *       201:
+ *         description: The Interaction was successfully added
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/PostRes'
+ *       401:
+ *         description: 
  *       500:
  *         description: Some server error
  */
+
 routes.post("/read", async (req, res) => {
   console.log(req.body);
-  const { title, story_id, user_id, read } = req.body;
-  await addInteractionRead(title, story_id, user_id, read);
-  res.send("Data inserted into Interaction db");
+  const { story_id, user_id, read } = req.body;
+  const result = await addInteractionRead(story_id, user_id, read);
+  if (result === 401) {
+    res.status(401).send("User not found Error!!!");
+  }
+ else {
+    const sender = { _id: result };
+    res.status(201).send(sender);
+  }
 });
 
 
@@ -135,46 +195,30 @@ routes.post("/read", async (req, res) => {
  *     tags: [UserInteraction]
  *     responses:
  *       200:
- *         description: The interaction was successfully added
+ *         description: The interaction was successfully sent
  *       500:
  *         description: Some server error
  */
+
 routes.get('/',  (req, res) => {
     const data =  getInteractions();
-    res.send(data);
+    res.status(200).send(data);
 });
 
 
 
- /**
- * @swagger
- * /read:
- *   post:
- *     summary: Acknowledges read to particular content and inserts entries into db
- *     tags: [UserInteraction]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/UserRead'
- *     responses:
- *       200:
- *         description: The interaction was successfully added
- *       500:
- *         description: Some server error
- */
+
 
 
 /**
  * @swagger
  * /:
  *  put:
- *    summary: Update the content by the id
+ *    summary: Update the interaction by the id
  *    tags: [UserInteraction]
  *    parameters:
  *      - in: query
- *        name: story_id
+ *        name: _id
  *        schema:
  *          type: string
  *        required: true
@@ -195,18 +239,61 @@ routes.get('/',  (req, res) => {
  *          type: string
  *        description: user_id for verification 
  *    responses:
- *      200:
- *        description: The content was updated
+ *      201:
+ *        description: The Interaction was successfully added
+ *        content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/PutUser'
  *      404:
- *        description: The content was not found
+ *        description: The interaction was not found
  *      500:
  *        description: Some error happened
  */
 
 routes.put('/',async (req, res) => {
-    const { story_id ,event, flag, user_id } = req.query;
-    updateInteraction(story_id, event, flag, user_id);
-    res.send("Data updated");
+    const { _id ,event, flag, user_id } = req.query;
+  const result = await updateInteraction(_id, event, flag, user_id);
+  if (result == 404) {
+    res.status(404).send("Interaction was not found! Error");
+  } else if (result == 500) {
+    res.status(500).send("Server Error!!");
+  } else if (result == 401) {
+    res.status(401).send("User not found !! Unauthorized access");
+  } else {
+    res.status(201).send(result);
+  }
+
 });
 
+/**
+ * @swagger
+ * /{id}:
+ *   delete:
+ *     summary: Remove the interaction by id
+ *     tags: [UserInteraction]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The UserInteraction id
+ * 
+ *     responses:
+ *       200:
+ *         description: The Interaction was deleted
+ *       404:
+ *         description: The Interaction was not found
+ */
+routes.delete('/:id', async (req, res) => {
+    const _id = req.params.id;
+  const user = await removeInteractionbyId(_id);
+   if (user == 404) {
+     res.status(404).send("UserInteraction not found");
+   } else {
+     res.status(200).send("UserInteraction has been deleted");
+   }
+
+});
 module.exports = routes;
